586a541a9a02b845162a88d0772237d15725aad1
2382032 <2382032@unai.edu>
2024-09-10 10:39:08 +0700

    Handson2

---
+++ Tugas.java
@@ -0,0 +1,7 @@
+public class Tugas {
+    public static void main(String[] args) {
+        //buatlah sebuah program yang mengambil 2 input number dan user
+        //program tersebut akan print out nilai yang paling besar
+
+    }
+}
