f187d35e29be929ffe021419241fc330007b51b3
2382032 <2382032@unai.edu>
2024-09-10 10:13:33 +0700

    Handson2

---
+++ BlockVsExpressionVsStatement.java
@@ -0,0 +1,13 @@
+public class BlockVsExpressionVsStatement {
+    public static void main(String[] args) {
+        // Ekspresi -> 10 + 10
+        // Ekspresi a = 20;
+        // Statement -> int a = 10 + 10;
+        int a = 10 + 10;
+        System.out.println("Misael Hendrikh Witjaksono");
+        System.out.println("Misael Hendrikh Witjaksono");
+        System.out.println("Misael Hendrikh Witjaksono");
+        System.out.println("Misael Hendrikh Witjaksono");
+
+    }
+}
