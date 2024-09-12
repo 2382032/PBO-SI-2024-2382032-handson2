d92d4b4daa9a3ae33144356f75948244579955ed
2382032 <2382032@unai.edu>
2024-09-12 19:26:58 +0700

    handson2

--- Tugas.java
+++ Tugas.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Tugas {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //buatlah sebuah program yang mengambil 2 input number dan user
         //program tersebut akan print out nilai yang paling besar
+
+        System.out.println("Masukan angka pertama:");
+        System.out.print("Masukkan angka pertama: ");
+        int angka1 = scanner.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        int angka2 = scanner.nextInt();
+        int nilaiTerbesar = (angka1 > angka2) ?angka1 : angka2;
+
+        System.out.println("Nilai yang paling besar adalah: " + nilaiTerbesar);
+        scanner.close();
+
+
 
     }
 }
